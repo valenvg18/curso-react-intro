68d36e6c15edc523e6d246a128e6d5421a938fbf
Valentina Villa <villagranadosvalen.18@gmail.com>
2023-05-28 08:48:42 -0500

    Se agregaron 2 props y se le atribuyeron al componente TodoCounter en la App

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,7 +1,7 @@
-function TodoCounter() {
+function TodoCounter({ total, completed }) {
     return (
       <h1>
-        Has completado 3 de 5 TODO'S
+        Has completado {completed} de {total} TODO'S
       </h1>
     );
 }
