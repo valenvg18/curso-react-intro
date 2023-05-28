8af5a768dcd52b8ffb801963dee157e74ea773f8
Valentina Villa <villagranadosvalen.18@gmail.com>
2023-05-28 10:14:22 -0500

    Por medio de las props, le agregamos propiedades al TodoItem

--- src/App.js
+++ src/App.js
@@ -4,25 +4,35 @@ import { TodoList } from "./TodoList";
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
+import React from 'react';
 
-
+const defaultTodos = [
+  { text: 'Cortar Cebolla', completed: true },
+  { text: 'Tomar curso de intro a React.js', completed: false },
+  { text: 'Estudiar ingles para la entrevista', completed: false },
+  { text: 'Estudiar conceptos basicos de Js y React', completed: true },
+];
 
 function App() {
   return (
-    
+    <React.Fragment>
 
       <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem 
+          key={todo.text} 
+          text={todo.text} 
+          completed = {todo.completed}
+          />
+        ))}
       </TodoList>
 
       <CreateTodoButton />
       
-    
+    </React.Fragment>  
   );
 }
 
