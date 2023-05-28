74643ba4965380e7312190ff6b9f0ae0c7e190ac
Valentina Villa <villagranadosvalen.18@gmail.com>
2023-05-28 08:20:15 -0500

    Creacion de los componentes de las tareas pendientes y estructura de nuestra App

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos React
-        </a>
-      </header>
+
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      <CreateTodoButton />
+      
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p> Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
