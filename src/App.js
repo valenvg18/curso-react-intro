855bc852bd6c34153dd14bef4e08c01f10dfbe6a
Valentina Villa <villagranadosvalen.18@gmail.com>
2023-05-28 23:38:42 -0500

    Searching for things

--- src/App.js
+++ src/App.js
@@ -23,6 +23,14 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
+
   console.log('Los usuarios buscan Todos de ' + searchValue);
 
   return (
@@ -38,7 +46,7 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
           key={todo.text} 
           text={todo.text} 
