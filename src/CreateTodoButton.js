4a50e0f81a93b94b0c8e676b3b4d1db7eb3e1072
Valentina Villa <villagranadosvalen.18@gmail.com>
2023-05-28 21:44:49 -0500

    Creation of events like onClick and onChange for the interaction to the users

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,13 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton() {
     return (
-      <button className="CreateTodoButton">
+      <button className="CreateTodoButton" onClick={
+        (event) => {
+          console.log('Le diste click')
+          console.log(event)
+          console.log(event.target)
+        }
+      }>
         +
       </button>
     );
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,15 @@ import './TodoSearch.css';
 
 function TodoSearch() {
     return (
-      <input placeholder="Buscar Tarea" className="TodoSearch"/>
+      <input 
+      placeholder="Buscar Tarea" 
+      className="TodoSearch"
+      onChange={(event) => {
+        console.log('Escribiste en el TodoSearch');
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}/>
     );
 }
 
