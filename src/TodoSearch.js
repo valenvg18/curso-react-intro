059d86b24bca5f14d5eb1ea3bf64157b7b14afa7
Valentina Villa <villagranadosvalen.18@gmail.com>
2023-05-28 22:22:06 -0500

    Creation of states on the TodoSearch file

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,23 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log('Los usuarios buscan Todos de ' + searchValue);
+
     return (
       <input 
       placeholder="Buscar Tarea" 
       className="TodoSearch"
+      value = {searchValue}
       onChange={(event) => {
-        console.log('Escribiste en el TodoSearch');
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.value);
+        setSearchValue(event.target.value);
+        // console.log('Escribiste en el TodoSearch');
+        // console.log(event);
+        // console.log(event.target);
+        // console.log(event.target.value);
       }}/>
     );
 }
